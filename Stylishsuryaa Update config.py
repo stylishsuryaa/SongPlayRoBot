66077b916afa0e45ec7bfd94d8b79bba1721661a
stylishsuryaa <83962899+stylishsuryaa@users.noreply.github.com>
2021-07-12 11:40:39 +0530

    Update Stylishsuryaa

---
+++ Stylishsuryaa Update config.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "6439635"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "cff5ae354fffc28e97739586d10c70a7" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1872576313:AAGz4847a38lrhbaQSkwsOHVdpPQairl1aA" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
